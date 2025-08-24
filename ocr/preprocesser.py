95df6e571c46aeb216583c8b321c87f6c73b00d6
[ Rahul Rao ] <okay.rahul015@gmail.com>
2025-08-24 14:44:31 +0530

    sample snippet added

---
+++ ocr/preprocesser.py
@@ -0,0 +1,8 @@
+import cv2
+
+def preprocess_image(img_path):
+    img = cv2.imread(img_path)
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    _, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
+    return thresh
+    
